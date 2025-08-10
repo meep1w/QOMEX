11b3065366c2cbebed70f707a558de51ff11aba8
meep1w <kerentsev05@inbox.ru>
2025-08-10 19:33:15 +0300

    Финальный тест еще один

--- models.py
+++ models.py
@@ -15,12 +15,14 @@ class User(Base):
     first_deposit = Column(Float, nullable=True)
     total_deposit = Column(Float, nullable=False, default=0.0)
     deposit_verified = Column(Boolean, default=False)
+    reset_token = Column(String(255), nullable=True)  # <-- добавлено
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def __repr__(self):
         return (f"<User(id={self.id}, login='{self.login}', click_id='{self.click_id}', "
                 f"trader_id='{self.trader_id}', total_deposit={self.total_deposit})>")
+
 
 class PostbackLog(Base):
     __tablename__ = "postbacks_log"
